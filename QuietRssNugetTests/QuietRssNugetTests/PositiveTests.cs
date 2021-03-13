6c44238066756fa8914b1f5f996483e5cdfe2e03
Andrew Chapman <chapman@chaptek.net>
2021-03-13 13:51:55 -0500

    Changed Dictionary to Tuple to accomodate multiple rss feeds per company

--- QuietRssNugetTests/QuietRssNugetTests/PositiveTests.cs
+++ QuietRssNugetTests/QuietRssNugetTests/PositiveTests.cs
@@ -20,9 +20,9 @@ namespace QuietRssNugetTests
         public void PositiveCanOpenKnownGoodRssFeed()
         {
             //arrange
-            var dictionary = new Dictionary<string, string>();
-            
-            dictionary.Add("Company 1", TestRssFeedHelper.MakeNewTestRssFeedWithDate(DateTime.UtcNow));
+            var dictionary = new List<Tuple<string, string>>();
+
+            dictionary.Add(new Tuple<string, string>("Company A", TestRssFeedHelper.MakeNewTestRssFeedWithDate(DateTime.UtcNow)));
 
             //act
             var ret = RssRetriever.GetQuietFeeds(dictionary, 0);
@@ -36,9 +36,9 @@ namespace QuietRssNugetTests
         public void PositiveCanOpenMultipleKnownGoodRssFeeds()
         {
             //arrange
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("Company A", TestRssFeedHelper.MakeNewTestRssFeedWithDate(DateTime.UtcNow));
-            dictionary.Add("Company B", TestRssFeedHelper.MakeNewTestRssFeedWithDate(DateTime.UtcNow));
+            var dictionary = new List<Tuple<string, string>>();
+            dictionary.Add(new Tuple<string, string>("Company A", TestRssFeedHelper.MakeNewTestRssFeedWithDate(DateTime.UtcNow)));
+            dictionary.Add(new Tuple<string, string>("Company B", TestRssFeedHelper.MakeNewTestRssFeedWithDate(DateTime.UtcNow)));
 
             //act
             var ret = RssRetriever.GetQuietFeeds(dictionary, 0);
@@ -51,9 +51,9 @@ namespace QuietRssNugetTests
         public void PositiveCanReturnMultipleRssFeedsForSameCompany()
         {
             //arrange
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("Company A", TestRssFeedHelper.MakeNewTestRssFeedWithDate(DateTime.UtcNow));
-            dictionary.Add("Company A", TestRssFeedHelper.MakeNewTestRssFeedWithDate(DateTime.UtcNow));
+            var dictionary = new List<Tuple<string, string>>();
+            dictionary.Add(new Tuple<string, string>("Company A", TestRssFeedHelper.MakeNewTestRssFeedWithDate(DateTime.UtcNow)));
+            dictionary.Add(new Tuple<string, string>("Company A", TestRssFeedHelper.MakeNewTestRssFeedWithDate(DateTime.UtcNow)));
 
             //act
             var ret = RssRetriever.GetQuietFeeds(dictionary, 0);
--- RssRetriever.cs
+++ RssRetriever.cs
@@ -8,17 +8,17 @@ namespace QuietRssNuget
 {
     public static class RssRetriever
     {
-        public static HashSet<RssSummary> GetQuietFeeds(Dictionary<string, string> companyFeeds, double? quietStreak = 5){
+        public static HashSet<RssSummary> GetQuietFeeds(List<Tuple<string, string>> companyFeeds, double? quietStreak = 5){
             var ret = new HashSet<RssSummary>();
             var quietStreakStartDate = DateTime.UtcNow.AddDays((double)quietStreak * -1);
 
             if(companyFeeds == null) return ret;
 
 
-            foreach (KeyValuePair<string, string> entry in companyFeeds) { 
+            foreach (Tuple<string, string> entry in companyFeeds) { 
                 
                 //open the feed
-                XmlReader feed = XmlReader.Create(entry.Value);
+                XmlReader feed = XmlReader.Create(entry.Item2);
                 var rssFeed = SyndicationFeed.Load(feed);
                 feed.Close();
 
@@ -26,7 +26,7 @@ namespace QuietRssNuget
                 {
                     DateTimeOffset lastUpdate = LastUpdateTime(rssFeed);
                     if(lastUpdate.DateTime.Date <= quietStreakStartDate.Date)
-                       ret.Add(new RssSummary(entry.Key, entry.Value, lastUpdate, 0 /*TODO FIX*/, rssFeed.Title.Text));
+                       ret.Add(new RssSummary(entry.Item1, entry.Item2, lastUpdate, 0 /*TODO FIX*/, rssFeed.Title.Text));
                 }
             }
             return ret;
