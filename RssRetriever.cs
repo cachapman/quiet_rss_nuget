0d04d420575cabc8d36119a416ffb150b4e16b07
Andrew Chapman <chapman@chaptek.net>
2021-03-13 11:52:38 -0500

    removed linq, set limt through build in functions, aded initial steps on feed

--- RssRetriever.cs
+++ RssRetriever.cs
@@ -1,27 +1,30 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.ServiceModel.Syndication;
+using System.Xml;
 
 namespace QuietRssNuget
 {
     public class RssRetriever
     {
         public HashSet<RssSummary> GetQuietFeeds(Dictionary<string, string> companyFeeds, int? quietStreak = 5, int? returnLimit = int.MaxValue){
-            var ret = new HashSet<RssSummary>();
+            var ret = new HashSet<RssSummary>((int)returnLimit);
             if(companyFeeds == null) return ret;
 
-            foreach (KeyValuePair<string, string> entry in companyFeeds{
+            foreach (KeyValuePair<string, string> entry in companyFeeds) { 
+                
                 //open the feed
-                //determine feed type
-                //format feed to understandble type
+                XmlReader feed = XmlReader.Create(entry.Value);
+                var rssFeed = SyndicationFeed.Load(feed);
+                feed.Close();
+
                 //get latest date
                 //compare to quiet streak
                 //if matches, add to list
                 //if at the return limit, break and return
             }
 
-            return ret.Take(returnLimit);
+            return ret;
 
         }
     }
