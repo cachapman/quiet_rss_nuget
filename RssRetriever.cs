c4cbf00d8358ad2fa3a5dccf86086d12f8adbdad
Andrew Chapman <chapman@chaptek.net>
2021-03-13 12:50:18 -0500

    added new scaffold for method to calculate last updated date used ctor for object rather than dynamic var added assumption that given dates is based on current date

--- RssRetriever.cs
+++ RssRetriever.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.ServiceModel.Syndication;
 using System.Xml;
+using System.Linq;
 
 namespace QuietRssNuget
 {
     public static class RssRetriever
     {
-        public static HashSet<RssSummary> GetQuietFeeds(Dictionary<string, string> companyFeeds, int? quietStreak = 5){
+        public static HashSet<RssSummary> GetQuietFeeds(Dictionary<string, string> companyFeeds, double? quietStreak = 5){
             var ret = new HashSet<RssSummary>();
+            var quietStreakStartDate = DateTime.UtcNow.AddDays((double)quietStreak * -1);
+
+
             if(companyFeeds == null) return ret;
 
             foreach (KeyValuePair<string, string> entry in companyFeeds) { 
@@ -20,23 +24,17 @@ namespace QuietRssNuget
 
                 if (rssFeed != null)
                 {
-                    ret.Add(new RssSummary
-                    {
-                        Company = entry.Key,
-                        RssUri = entry.Value,
-                        LastPost = DateTime.Now, //TODO FIX
-                        QuietStreak = 0, //TODO FIX
-                        Title = rssFeed.Title.Text
-                    });
+                    DateTimeOffset lastUpdate = LastUpdateTime(rssFeed);
+                    if(lastUpdate.DateTime.Date <= quietStreakStartDate.Date)
+                       ret.Add(new RssSummary(entry.Key, entry.Value, lastUpdate, 0 /*TODO FIX*/, rssFeed.Title.Text));
                 }
-                //get latest date
-                //compare to quiet streak
-                //if matches, add to list
-              
             }
-
             return ret;
+        }
 
+        private static DateTimeOffset LastUpdateTime(SyndicationFeed rssFeed)
+        {
+            return new DateTimeOffset();
         }
     }
 }
--- RssSummary.cs
+++ RssSummary.cs
@@ -7,10 +7,21 @@ namespace QuietRssNuget
     //TODO: Ask on longevity of this code
     public class RssSummary
     {
+        public RssSummary(string company, string rssUri, DateTimeOffset lastUpdate, int quietStreak, string title)
+        {
+            this.Company = company;
+            this.RssUri = rssUri;
+            this.LastPostDateUTC = lastUpdate.UtcDateTime;
+            this.QuietStreak = quietStreak; 
+            this.Title = Title;
+        }
+
+      
         public string Company {get;set;}
         public string Title { get; set; }
         public string RssUri { get; set; }
         public int QuietStreak {get;set;}
-        public DateTime LastPost { get; set; }
+        public DateTime LastPostDateUTC { get; set; }
+
     }
 }
