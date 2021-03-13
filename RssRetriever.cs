c4c873ed8f139a7e098f74808ac4b894f2a72739
Andrew Chapman <chapman@chaptek.net>
2021-03-13 11:07:49 -0500

    Wrote psuedo code for quiet searcher using linq returns limit of items todo comment on model

--- RssRetriever.cs
+++ RssRetriever.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.ServiceModel.Syndication;
 
@@ -7,7 +8,21 @@ namespace QuietRssNuget
     public class RssRetriever
     {
         public HashSet<RssSummary> GetQuietFeeds(Dictionary<string, string> companyFeeds, int? quietStreak = 5, int? returnLimit = int.MaxValue){
-            return null;
+            var ret = new HashSet<RssSummary>();
+            if(companyFeeds == null) return ret;
+
+            foreach (KeyValuePair<string, string> entry in companyFeeds{
+                //open the feed
+                //determine feed type
+                //format feed to understandble type
+                //get latest date
+                //compare to quiet streak
+                //if matches, add to list
+                //if at the return limit, break and return
+            }
+
+            return ret.Take(returnLimit);
+
         }
     }
 }
--- RssSummary.cs
+++ RssSummary.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace QuietRssNuget
 {
+    //I have a feeling we may need to make this more generic
+    //But, unsure of market demand for expansion, since the ask is straight forward.
+    //TODO: Ask on longevity of this code
     public class RssSummary
     {
         public string Company {get;set;}
